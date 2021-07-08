915925aa183c73ccdfc306bd44773e7da19b38e5
juampiarias <juaarias@itba.edu.ar>
2021-07-08 20:19:09 -0300

    Todas figuras terminadas menos linea

--- src/backend/model/Figure.java
+++ src/backend/model/Figure.java
@@ -1,14 +1,48 @@
 package backend.model;
 
 public abstract class Figure {
-    protected abstract Point[] getPoints();
 
-    public void move(double x, double y) {
-        for(Point p : this.getPoints()) {
-            p.setX(p.getX() + x);
-            p.setY(p.getY() + y);
-        }
+    private final Point topLeft, bottomRight;
+    private double xDiameter, yDiameter;
+
+    public Figure(Point topLeft, Point bottomRight) {
+        this.bottomRight = bottomRight;
+        this.topLeft = topLeft;
+        xDiameter = bottomRight.getX() - topLeft.getX();
+        yDiameter = bottomRight.getY() - topLeft.getY();
     }
 
-    public abstract boolean belongs(Point p);
+    public void move(double x, double y) {
+        topLeft.setX(topLeft.getX() + x);
+        topLeft.setY(topLeft.getY() + y);
+    }
+
+    public boolean belongs(Point p) {
+        return Double.compare(p.getX(), getTopLeft().getX()) >= 0 && Double.compare(p.getX(), getBottomRight().getX()) <= 0 &&
+                Double.compare(p.getY(), getTopLeft().getY()) >= 0 && Double.compare(p.getY(), getBottomRight().getY()) <= 0;
+    }
+
+    public void setXDiameter(double xDiameter) {
+        this.xDiameter = xDiameter;
+    }
+
+    public void setYDiameter(double yDiameter) {
+        this.yDiameter = yDiameter;
+    }
+
+    public double getXDiameter() {
+        return xDiameter;
+    }
+
+    public double getYDiameter() {
+        return yDiameter;
+    }
+
+    public Point getTopLeft() {
+        return topLeft;
+    }
+
+    public Point getBottomRight() {
+        return bottomRight;
+    }
 }
