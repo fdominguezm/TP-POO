831bd7ce3a701bbdc5e5793b22ad90d0772594e3
frandchule <fdominguez@itba.edu.ar>
2021-07-18 16:50:21 -0300

    Todo funciona, cambios en el PaintPane y CanavsState

--- frontend/MainFrame.java
+++ frontend/MainFrame.java
@@ -7,7 +7,7 @@ public class MainFrame extends VBox {
     public MainFrame() {
         getChildren().add(new AppMenuBar());
         StatusPane statusPane = new StatusPane();
-        getChildren().add(new PaintPaneAux(statusPane));
+        getChildren().add(new PaintPane(statusPane));
         getChildren().add(statusPane);
     }
 
