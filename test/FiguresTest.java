bf35ab03b55072b1b74ff942ae46d68c4cfaab98
gelewaut <gelewaut@itba.edu.ar>
2021-07-18 22:04:44 -0300

    JUnit Finalizado

--- test/FiguresTest.java
+++ test/FiguresTest.java
@@ -1,78 +1,129 @@
-package test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import backend.model.*;
 import org.junit.jupiter.api.*;
 
 public class FiguresTest {
 
-    Point tpRect = new Point(0.5, 3.1);
-    Point btmRect = new Point(9.8, 15.9);
-    Point tpOval = new Point(2.5, 4.1);
-    Point btmOval = new Point(15.8, 20.9);
-    double radius = 6.0;
 
-    Figure rectangle = new Rectangle(new Point(0.5, 3.1), new Point(9.8, 15.9));
-    Figure square = new Square(new Point(0.5, 3.1), new Point(9.8, 15.9));
-    Figure ellipse = new Ellipse(new Point(2.5, 4.1), new Point(15.8, 20.9));
-    Figure circle = new Circle(new Point(2.5, 4.1), 6.0);
-    Figure line = new Line(new Point(2.5, 4.1), new Point(15.8, 20.9));
+    Figure rectangle = new Rectangle(new Point(5.0, 5.0), new Point(10.0, 15.0));
+    Figure square = new Square(new Point(5.0, 5.0), new Point(10.0, 15.0));
+    Figure ellipse = new Ellipse(new Point(5.0, 5.0), new Point(10.0, 15.0));
+    Figure circle = new Circle(new Point(7.5, 7.5), 2.0);
+    Figure line = new Line(new Point(5.0, 5.0), new Point(10.0, 15.0));
 
 
-    Point p1 = new Point(2.4, 6.1);
-    Point p2 = new Point(0.0, 1.1);
-    Point p3 = new Point(21.1, 22.1);
+    Point p1 = new Point(7.0, 7.0);
+    Point p2 = new Point(2.5, 2.5);
+    Point p3 = new Point(20.0, 20.0);
+
+    Point p4 = new Point(50.0,50.0);
 
      //poner belongs(p1), belongs(p1,p2), move, getxdiameter, getydiameter, gettopleft, get topright
     //para todas las figuras
     @Test
     void circle () {
-        assert(circle.belongs(p1));
-        assert(circle.belongs(p2, p3));
+        assertTrue(circle.belongs(p1));
+        assertTrue(!circle.belongs(p2));
+        assertTrue(!circle.belongs(p3));
+        assertTrue(!circle.belongs(new Point(6.0,6.0)));
+
+        assertTrue(!circle.belongs(p1,p3));
+        assertTrue(circle.belongs(p2, p3));
+
+        double radius = 2.0;
+
         assertEquals(2*radius,circle.getXDiameter());
         assertEquals(2*radius,circle.getYDiameter());
-        assert(circle.getTopLeft().equals(new Point(2.5-radius, 4.1-radius)));
-        assert(circle.getBottomRight().equals(new Point(9.8+radius, 15.9+radius)));
+
+        assertTrue(circle.getTopLeft().equals(new Point(7.5-radius, 7.5-radius)));
+        assertTrue(circle.getBottomRight().equals(new Point(7.5+radius, 7.5+radius)));
+
+        circle.move(25.0,25.0);
+
+        assertTrue(!circle.belongs(p2, p3));
+        assertTrue(circle.belongs(p3, p4));
     }
 
     @Test
     void ellipse () {
-        assert(ellipse.belongs(p1));
-        assert(ellipse.belongs(p2, p3));
-        assertEquals(13.3,ellipse.getXDiameter());
-        assertEquals(16.8,ellipse.getYDiameter());
-        assert(ellipse.getTopLeft().equals(new Point(2.5, 4.1)));
-        assert(ellipse.getBottomRight().equals( new Point(15.8, 20.9)));
+        assertTrue(ellipse.belongs(p1));
+        assertTrue(!ellipse.belongs(p2));
+        assertTrue(!ellipse.belongs(p3));
+        assertTrue(!ellipse.belongs(new Point(5.0,5.0)));
+
+        assertTrue(ellipse.belongs(p2, p3));
+
+        assertEquals(5.0,ellipse.getXDiameter());
+        assertEquals(10.0,ellipse.getYDiameter());
+
+        assertTrue(ellipse.getTopLeft().equals(new Point(5.0, 5.0)));
+        assertTrue(ellipse.getBottomRight().equals( new Point(10.0, 15.0)));
+
+        ellipse.move(25.0,25.0);
+
+        assertTrue(!ellipse.belongs(p2,p3));
+        assertTrue(ellipse.belongs(p3,p4));
     }
 
     @Test
     void line () {
-        assert(!line.belongs(p1));
-        assert(line.belongs(p2,p3));
-        assertEquals(13.3,line.getXDiameter());
-        assertEquals(16.8,line.getYDiameter());
-        assert(line.getTopLeft().equals(new Point(2.5, 4.1)));
-        assert(line.getBottomRight().equals(new Point(15.8, 20.9)));
+        assertTrue(!line.belongs(p1));
+        assertTrue(!line.belongs(p2));
+        assertTrue(!line.belongs(p3));
+        assertTrue(!line.belongs(new Point(5.0,5.0)));
+
+        assertTrue(line.belongs(p2, p3));
+
+        assertTrue(line.getTopLeft().equals(new Point(5.0, 5.0)));
+        assertTrue(line.getBottomRight().equals( new Point(10.0, 15.0)));
+
+        line.move(25.0,25.0);
+
+        assertTrue(!line.belongs(p2,p3));
+        assertTrue(line.belongs(p3,p4));
     }
 
     @Test
     void rectangle () {
-        assert(rectangle.belongs(p1));
-        assert(rectangle.belongs(p2, p3));
-        assertEquals(9.3,rectangle.getXDiameter());
-        assertEquals(12.8,rectangle.getYDiameter());
-        assert(rectangle.getTopLeft().equals(new Point(0.5, 3.1)));
-        assert(rectangle.getBottomRight().equals(new Point(9.8, 15.9)));
+        assertTrue(rectangle.belongs(p1));
+        assertTrue(!rectangle.belongs(p2));
+        assertTrue(!rectangle.belongs(p3));
+        assertTrue(rectangle.belongs(new Point(5.0,5.0)));
+
+        assertTrue(rectangle.belongs(p2, p3));
+
+        assertEquals(5.0,rectangle.getXDiameter());
+        assertEquals(10.0,rectangle.getYDiameter());
+
+        assertTrue(rectangle.getTopLeft().equals(new Point(5.0, 5.0)));
+        assertTrue(rectangle.getBottomRight().equals( new Point(10.0, 15.0)));
+
+        rectangle.move(25.0,25.0);
+
+        assertTrue(!rectangle.belongs(p2,p3));
+        assertTrue(rectangle.belongs(p3,p4));
     }
 
     @Test
     void square () {
-        assert(square.belongs(p1));
-        assert(square.belongs(p2, p3));
-        assertEquals(9.3,square.getXDiameter());
-        assertEquals(12.8,square.getYDiameter());
-        assert(square.getTopLeft().equals(new Point(0.5, 3.1)));
-        assert(square.getBottomRight().equals(new Point(9.8, 15.9)));
+        assertTrue(square.belongs(p1));
+        assertTrue(!square.belongs(p2));
+        assertTrue(!square.belongs(p3));
+        assertTrue(square.belongs(new Point(14.0,14.0)));
+
+        assertTrue(square.belongs(p2, p3));
+
+        assertEquals(10.0,square.getXDiameter());
+        assertEquals(10.0,square.getYDiameter());
+
+        assertTrue(square.getTopLeft().equals(new Point(5.0, 5.0)));
+        assertTrue(square.getBottomRight().equals( new Point(15.0, 15.0)));
+
+        square.move(25.0,25.0);
+
+        assertTrue(!square.belongs(p2,p3));
+        assertTrue(square.belongs(p3,p4));
     }
 }
--- test/PointTest.java
+++ test/PointTest.java
@@ -1,9 +1,7 @@
-package test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import backend.model.Point;
-import org.junit.Test;
 import org.junit.jupiter.api.*;
 
 public class PointTest {
